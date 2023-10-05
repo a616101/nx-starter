7a96ec9e8bd4719245528218e69b86eea7568634
a616101 <a616101@gmail.com>
2023-10-05 15:27:18 +0800

    Initial commit

---
+++ libs/client-d3-charts/src/lib/d3-charts.module.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AppGaugeChartComponent } from './components';
+import { AppBarChartComponent } from './components/bar-chart/bar-chart.component';
+import { AppChartExamplesComponent } from './components/chart-examples/chart-examples.component';
+import { AppChartExamplesBarComponent } from './components/chart-examples-bar/chart-examples-bar.component';
+import { AppChartExamplesForceDirectedComponent } from './components/chart-examples-force-directed/chart-examples-force-directed.component';
+import { AppChartExamplesGaugeComponent } from './components/chart-examples-gauge/chart-examples-gauge.component';
+import { AppChartExamplesLineComponent } from './components/chart-examples-line/chart-examples-line.component';
+import { AppChartExamplesPieComponent } from './components/chart-examples-pie/chart-examples-pie.component';
+import { AppChartExamplesRadaraComponent } from './components/chart-examples-radar/chart-examples-radar.component';
+import { AppForceDirectedChartComponent } from './components/force-directed-chart/force-directed-chart.component';
+import { AppLineChartComponent } from './components/line-chart/line-chart.component';
+import { AppPieChartComponent } from './components/pie-chart/pie-chart.component';
+import { AppRadarChartComponent } from './components/radar-chart/radar-chart.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    AppGaugeChartComponent,
+    AppPieChartComponent,
+    AppRadarChartComponent,
+    AppForceDirectedChartComponent,
+    AppBarChartComponent,
+    AppLineChartComponent,
+    AppChartExamplesComponent,
+    AppChartExamplesLineComponent,
+    AppChartExamplesRadaraComponent,
+    AppChartExamplesBarComponent,
+    AppChartExamplesPieComponent,
+    AppChartExamplesGaugeComponent,
+    AppChartExamplesForceDirectedComponent,
+  ],
+  exports: [
+    AppGaugeChartComponent,
+    AppPieChartComponent,
+    AppRadarChartComponent,
+    AppForceDirectedChartComponent,
+    AppBarChartComponent,
+    AppLineChartComponent,
+    AppChartExamplesComponent,
+  ],
+})
+export class AppD3ChartsModule {}
---
+++ libs/client-d3-charts/src/lib/providers/d3-chart-factory.provider.ts
@@ -0,0 +1,31 @@
+import { InjectionToken } from '@angular/core';
+
+import { drawBarChart } from '../util/bar-chart.util';
+import { drawForceDirectedChart } from '../util/force-directed-chart.util';
+import { drawGaugeChart } from '../util/gauge-chart.util';
+import { drawLineChart } from '../util/line-chart.util';
+import { drawPieChart } from '../util/pie-chart.util';
+import { drawRadarChart } from '../util/radar-chart.util';
+
+export interface ID3ChartFactory {
+  drawGaugeChart: typeof drawGaugeChart;
+  drawPieChart: typeof drawPieChart;
+  drawRadarChart: typeof drawRadarChart;
+  drawBarChart: typeof drawBarChart;
+  drawLineChart: typeof drawLineChart;
+  drawForceDirectedChart: typeof drawForceDirectedChart;
+}
+
+export const d3ChartFactory = (): ID3ChartFactory => ({
+  drawGaugeChart,
+  drawPieChart,
+  drawRadarChart,
+  drawBarChart,
+  drawLineChart,
+  drawForceDirectedChart,
+});
+
+export const D3_CHART_FACTORY = new InjectionToken('D3_CHART_FACTORY', {
+  providedIn: 'root',
+  factory: d3ChartFactory,
+});
